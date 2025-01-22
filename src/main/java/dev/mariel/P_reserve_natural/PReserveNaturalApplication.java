2cb3ef8da1fb270c2441c1ddd20363c7a72ed62f
Codebreakerblack <blancohmariel@gmail.com>
2025-01-22 10:41:29 +0100

    add branch dev

---
+++ src/main/java/dev/mariel/P_reserve_natural/PReserveNaturalApplication.java
@@ -0,0 +1,13 @@
+package dev.mariel.P_reserve_natural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PReserveNaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PReserveNaturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mariel/P_reserve_natural/PReserveNaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mariel.P_reserve_natural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PReserveNaturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
